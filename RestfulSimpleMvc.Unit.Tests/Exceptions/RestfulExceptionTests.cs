712b9219bbf63949788c1f928b7e4fa1a4ca77cc
Matthew Butt <matthew.butt@7digital.com>
2011-07-20 17:57:04 +0100

    RestfulException tests

--- RestfulSimpleMvc.Unit.Tests/Exceptions/RestfulExceptionTests.cs
+++ RestfulSimpleMvc.Unit.Tests/Exceptions/RestfulExceptionTests.cs
@@ -1,7 +1,30 @@
+using System.Net;
+using NUnit.Framework;
+using RestfulSimpleMvc.Core.Exceptions;
+
 namespace RestfulSimpleMvc.Unit.Tests.Exceptions
 {
+	[TestFixture]
 	public class RestfulExceptionTests
 	{
-		
+		[Test]
+		public void AmbiguousReturnsExceptionWithCorrectStatusCode() {
+			var exception = RestfulException.Ambiguous();
+			Assert.That(exception.HttpStatusCode, Is.EqualTo(HttpStatusCode.Ambiguous));
+		}
+
+		[Test]
+		public void BadGatewayReturnsExceptionWithCorrectStatusCode()
+		{
+			var exception = RestfulException.BadGateway();
+			Assert.That(exception.HttpStatusCode, Is.EqualTo(HttpStatusCode.BadGateway));
+		}
+
+		[Test]
+		public void NotFoundReturnsExceptionWithCorrectStatusCode()
+		{
+			var exception = RestfulException.NotFound();
+			Assert.That(exception.HttpStatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+		}
 	}
 }
