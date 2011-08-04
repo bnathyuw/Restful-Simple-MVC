88f4fbd80d2c1260bc1b049b44b1ad60fc2d3fb9
Matthew Butt <matthew.butt@7digital.com>
2011-08-04 11:50:07 +0100

    Created RouteCollectionExtensions and MapResource. IoC is a bit screwed up here.

---
+++ RestfulSimpleMvc.Core/Routes/RouteCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Routing;
+using RestfulSimpleMvc.Core.Configuration;
+
+namespace RestfulSimpleMvc.Core.Routes
+{
+	public static class RouteCollectionExtensions
+	{
+		private static readonly IActionMapper _actionMapper;
+		private static readonly IResponseTypeMapper _responseTypeMapper;
+
+		static RouteCollectionExtensions() {
+			var container = StructureMapBootstrapper.Container;
+			_actionMapper = container.GetInstance<IActionMapper>();
+			_responseTypeMapper = container.GetInstance<IResponseTypeMapper>();
+		}
+
+		public static void MapResource(this RouteCollection routeCollection, string url, string controller) {
+		 	routeCollection.Add(new RestfulRoute(url, controller, _responseTypeMapper, _actionMapper));
+		 }
+	}
+}
--- RestfulSimpleMvc.Web/Global.asax.cs
+++ RestfulSimpleMvc.Web/Global.asax.cs
@@ -2,47 +2,29 @@
 using System.Web.Routing;
 using RestfulSimpleMvc.Core.Configuration;
 using RestfulSimpleMvc.Core.Routes;
-using StructureMap;
 
 namespace RestfulSimpleMvc.Web
 {
 	public class MvcApplication : System.Web.HttpApplication
 	{
-	    private static readonly IContainer _container;
-	    private static readonly IResponseTypeMapper _responseTypeMapper;
-		private static readonly IActionMapper _actionMapper;
-
-	    static MvcApplication() {
-	        _container = StructureMapBootstrapper.Container;
-	        _responseTypeMapper = _container.GetInstance<IResponseTypeMapper>();
-	    	_actionMapper = _container.GetInstance<IActionMapper>();
-	    }
-
-	    private static void RegisterGlobalFilters(GlobalFilterCollection filters)
-		{
+		private static void RegisterGlobalFilters(GlobalFilterCollection filters) {
 			filters.Add(new HandleErrorAttribute());
 		}
 
-		private static void RegisterRoutes(RouteCollection routes)
-		{
+		private static void RegisterRoutes(RouteCollection routes) {
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.Add(RestfulRoute("", "Home"));
-            routes.Add(RestfulRoute("Exceptions/{httpStatusCode}", "Exception"));
-            routes.Add(RestfulRoute("Broken", "Broken"));
-            routes.Add(RestfulRoute("Addresses/{id}", "Address"));
-            routes.Add(RestfulRoute("Addresses", "Addresses"));
+			routes.MapResource("", "Home");
+			routes.MapResource("Exceptions/{httpStatusCode}", "Exception");
+			routes.MapResource("Broken", "Broken");
+			routes.MapResource("Addresses/{id}", "Address");
+			routes.MapResource("Addresses", "Addresses");
 		}
 
-	    private static RestfulRoute RestfulRoute(string url, string controller) {
-	        return new RestfulRoute(url, controller, _responseTypeMapper, _actionMapper);
-	    }
-
-	    protected void Application_Start()
-		{
+		protected void Application_Start() {
 			AreaRegistration.RegisterAllAreas();
 
-		    DependencyResolver.SetResolver(new StructureMapDependencyResolver(_container));
+			DependencyResolver.SetResolver(new StructureMapDependencyResolver(StructureMapBootstrapper.Container));
 			RegisterGlobalFilters(GlobalFilters.Filters);
 			RegisterRoutes(RouteTable.Routes);
 		}
