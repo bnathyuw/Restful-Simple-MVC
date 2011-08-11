a37f974bda14caad3bb2dad0541bd403eb21e560
Matthew Butt <matthew.butt@7digital.com>
2011-08-11 18:00:11 +0100

    Started using WebRequestWrapper

---
+++ RestfulSimpleMvc.Acceptance.Tests/ResponseTypeTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using NUnit.Framework;
+using WebRequestWrapper;
+
+namespace RestfulSimpleMvc.Acceptance.Tests
+{
+	[TestFixture]
+	public class ResponseTypeTests
+	{
+		private readonly WebRequester _webRequester = new WebRequester(new ConsoleRequestLogger());
+
+		[Test]
+		public void IfNoCallbackIsSpecifiedWithJsonPInQueryThenReturnBadRequest() {
+			var response = _webRequester.Get("http://localhost/restful-simple-mvc/.jsonp");
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+		}
+
+		[Test]
+		public void IfCallbackIsSpecifiedWithJsonPInQueryThenReturnOK() {
+			var response = _webRequester.Get("http://localhost/restful-simple-mvc/.jsonp?callback=callback");
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+		}
+	}
+}
