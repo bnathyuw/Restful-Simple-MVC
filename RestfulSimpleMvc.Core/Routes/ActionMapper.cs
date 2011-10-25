508fff2e64ad4a1f9a9d73ee3454cff11f300b8e
Matthew Butt <matthew@matthewbutt.com>
2011-10-25 21:43:19 +0100

    Allow querystring _action to override httpMethod@

--- RestfulSimpleMvc.Core/Routes/ActionMapper.cs
+++ RestfulSimpleMvc.Core/Routes/ActionMapper.cs
@@ -10,6 +10,9 @@ namespace RestfulSimpleMvc.Core.Routes
 			if (httpMethod.ToUpperInvariant() == "POST") {
 				httpMethod = httpContext.Request.Form["_action"] ?? httpMethod;
 			}
+			else if (httpMethod.ToUpperInvariant() == "GET") {
+				httpMethod = httpContext.Request.QueryString["_action"] ?? httpMethod;
+			}
 			routeData.Values.Add("action", httpMethod);
 		}
 	}
