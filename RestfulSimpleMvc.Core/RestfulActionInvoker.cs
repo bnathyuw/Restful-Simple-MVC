f7eda741ce42f7aafd4c3cfc72604962b1de5ba8
Matthew Butt <matthew@matthewbutt.com>
2011-10-26 00:07:20 +0100

    Check JsonP has callback

--- RestfulSimpleMvc.Core/RestfulActionInvoker.cs
+++ RestfulSimpleMvc.Core/RestfulActionInvoker.cs
@@ -3,19 +3,23 @@ using System.Linq;
 using System.Web.Mvc;
 using RestfulSimpleMvc.Core.Exceptions;
 using RestfulSimpleMvc.Core.Results;
+using RestfulSimpleMvc.Core.Routes;
 
 namespace RestfulSimpleMvc.Core
 {
-	public class RestfulActionInvoker:ControllerActionInvoker
-	{
+	public class RestfulActionInvoker : ControllerActionInvoker {
 		private readonly ITypedResultFactory _typedResultFactory;
 
 		public RestfulActionInvoker(ITypedResultFactory typedResultFactory) {
 			_typedResultFactory = typedResultFactory;
 		}
 
-		protected override ActionResult CreateActionResult(ControllerContext controllerContext, ActionDescriptor actionDescriptor, object actionReturnValue)
-		{
+		protected override AuthorizationContext InvokeAuthorizationFilters(ControllerContext controllerContext, IList<IAuthorizationFilter> filters, ActionDescriptor actionDescriptor) {
+			CheckJsonPHasCallback(controllerContext);
+			return base.InvokeAuthorizationFilters(controllerContext, filters, actionDescriptor);
+		}
+
+		protected override ActionResult CreateActionResult(ControllerContext controllerContext, ActionDescriptor actionDescriptor, object actionReturnValue) {
 			if (actionReturnValue == null)
 				return new EmptyResult();
 
@@ -24,20 +28,24 @@ namespace RestfulSimpleMvc.Core
 			return actionResult;
 		}
 
-		protected override ExceptionContext InvokeExceptionFilters(ControllerContext controllerContext, IList<IExceptionFilter> filters, System.Exception exception)
-		{
+		protected override ExceptionContext InvokeExceptionFilters(ControllerContext controllerContext, IList<IExceptionFilter> filters, System.Exception exception) {
 			var context = new ExceptionContext(controllerContext, exception);
-			foreach (var filter in filters.Reverse())
-			{
+			foreach (var filter in filters.Reverse()) {
 				filter.OnException(context);
 			}
 			SetExceptionResult(controllerContext, context);
 			return context;
 		}
 
+		private static void CheckJsonPHasCallback(ControllerContext controllerContext) {
+			var routeValueDictionary = controllerContext.RouteData.Values;
+
+			if ((ResponseType) routeValueDictionary["responseType"] == ResponseType.JsonP && routeValueDictionary["callback"] == null) throw RestfulException.BadRequest();
+		}
+
 		private void SetExceptionResult(ControllerContext controllerContext, ExceptionContext context) {
-			var restfulException = context.Exception as RestfulException 
-				?? RestfulException.InternalServerError("An internal server error has occurred", context.Exception);
+			var restfulException = context.Exception as RestfulException
+			                       ?? RestfulException.InternalServerError("An internal server error has occurred", context.Exception);
 			context.Result = _typedResultFactory.Build(controllerContext, restfulException, "Exception");
 			context.ExceptionHandled = true;
 		}
